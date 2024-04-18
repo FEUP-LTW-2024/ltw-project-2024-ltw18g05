89b0ff84ed7ccba61bff398b5c04ff5b070f1427
Afonso Castro <up202208026@up.pt>
2024-04-18 16:34:21 +0100

    Project organized

---
+++ pages/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Voyager</title>
+        <meta charset="UTF-8">
+        <link href="/css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <div id="title_slogan">
+                <h1><a href="index.php">Voyager</a></h1>
+                <h2><a href="index.php">Buy, Sell, Explore</a></h2>
+            </div>
+            <div id=search>
+                <input type="search" name="search" placeholder="Explore Voyager...">
+                <button formaction="login.php" formmethod="post" type="submit">Search</button>
+            </div>
+            <div id="login_signup_image">
+                <a href="register_page.php">Login</a>
+                <a href="login_page.php">Sign Up</a>
+                <img src="/images/anonymous.png">
+            </div>
+        </header>
+        
+        <nav id="nav_categories">
+            <input type="checkbox" id="hamburger"> 
+            <label class="hamburger" for="hamburger"></label>
+            <ul>
+                <li><a href="index.php">Eletronics</a></li>
+                <li><a href="index.php">Clothing</a></li>
+                <li><a href="index.php">Books</a></li>
+                <li><a href="index.php">Furniture</a></li>
+                <li><a href="index.php">Toys</a></li>
+            </ul>
+        </nav>
+
+        <section id=featured>
+            <h1><a href="item.html">Featured Items</a></h1>
+            <section id=featured_articles>
+                <article>
+                    <h1><a href="item.php">Quisque a dapibus magna, non scelerisque</a></h1>
+                    <img src="/images/default.jpg" alt="default">
+                    <footer>
+                        <span class="price"><a href="item.php">19.99€</a></span>
+                        <span class="condition"><a href="item.php">Very Good</a></span>
+                    </footer>
+                </article>
+                <article>
+                    <h1><a href="item.php">item Placement</a></h1>
+                    <img src="/images/default.jpg" alt="default">
+                    <footer>
+                        <span class="price"><a href="item.php">19.99€</a></span>
+                        <span class="condition"><a href="item.php">Very Good</a></span>
+                    </footer>
+                </article>
+                <article>
+                    <h1><a href="item.php">item Placement</a></h1>
+                    <img src="/images/default.jpg" alt="default">
+                    <footer>
+                        <span class="price"><a href="item.php">19.99€</a></span>
+                        <span class="condition"><a href="item.php">Very Good</a></span>
+                    </footer>
+                </article>
+            </section>
+        </section>
+        
+        <section id=items>
+            <h1><a href="item.html">Recently Posted</a></h1>
+            <section id=items_articles>
+                <article>
+                    <h1><a href="item.html">Quisque a dapibus magna, non scelerisque</a></h1>
+                    <img src="/images/default.jpg" alt="default">
+                    <footer>
+                        <span class="price"><a href="item.php">19.99€</a></span>
+                        <span class="condition"><a href="item.php">Very Good</a></span>
+                    </footer>
+                </article>
+                <article>
+                    <h1><a href="item.php">Item Placement</a></h1>
+                    <img src="/images/default.jpg" alt="default">
+                    <footer>
+                        <span class="price"><a href="item.php">19.99€</a></span>
+                        <span class="condition"><a href="item.php">Very Good</a></span>
+                    </footer>
+                </article>
+                <article>
+                    <h1><a href="item.php">Item Placement</a></h1>
+                    <img src="/images/default.jpg" alt="default">
+                    <footer>
+                        <span class="price"><a href="item.php">19.99€</a></span>
+                        <span class="condition"><a href="item.php">Very Good</a></span>
+                    </footer>
+                </article>
+                <article>
+                    <h1><a href="item.php">Item Placement</a></h1>
+                    <img src="/images/default.jpg" alt="default">
+                    <footer>
+                        <span class="price"><a href="item.php">19.99€</a></span>
+                        <span class="condition"><a href="item.php">Very Good</a></span>
+                    </footer>
+                </article>
+                <article>
+                    <h1><a href="item.php">Item Placement</a></h1>
+                    <img src="/images/default.jpg" alt="default">
+                    <footer>
+                        <span class="price"><a href="item.php">19.99€</a></span>
+                        <span class="condition"><a href="item.php">Very Good</a></span>
+                    </footer>
+                </article>
+            </section>
+        </section>
+
+        <footer>
+            <p>&copy; Voyager International Commerce Group</p>
+        </footer>
+    </body>
+</html>
