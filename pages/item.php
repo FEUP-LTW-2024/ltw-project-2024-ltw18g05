1cc611616ea22db881a68111d9ac3b7114eceeb7
Pedro Santos <up202205900@up.pt>
2024-04-23 11:36:34 +0100

    Item page

--- pages/item.php
+++ pages/item.php
@@ -1,41 +1,48 @@
 <!DOCTYPE html>
-<head>
-    <title>Voyager</title>
-    <meta charset="UTF-8">
-    <link href="css/style.css" rel="stylesheet">
-</head>
-<body>
-    <header>
-        <div id="title_slogan">
-            <h1><a href="index.php">Voyager</a></h1>
-            <h2><a href="index.php">Buy, Sell, Explore</a></h2>
-        </div>
-        <div id=search>
-            <input type="search" name="search" placeholder="Explore Voyager...">
-            <button formaction="login.php" formmethod="post" type="submit">Search</button>
-        </div>
-        <div id="login_signup_image">
-            <a href="register_page.php">Login</a>
-            <a href="login_page.php">Sign Up</a>
-            <img src="images/anonymous.png">
-        </div>
-    </header>
-    
-    <nav id="nav_categories">
-        <input type="checkbox" id="hamburger"> 
-        <label class="hamburger" for="hamburger"></label>
-        <ul>
-            <li><a href="index.php">Eletronics</a></li>
-            <li><a href="index.php">Clothing</a></li>
-            <li><a href="index.php">Books</a></li>
-            <li><a href="index.php">Furniture</a></li>
-            <li><a href="index.php">Toys</a></li>
-        </ul>
-    </nav>
+<html>
+    <head>
+        <title>Voyager</title>
+        <meta charset="UTF-8">
+        <link href="/css/item.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <div id="title_slogan">
+                <h1><a href="index.php">Voyager</a></h1>
+                <h2><a href="index.php">Buy, Sell, Explore</a></h2>
+            </div>
+            <div id=search>
+                <input type="search" name="search" placeholder="Explore Voyager...">
+                <button formaction="login.php" formmethod="post" type="submit">Search</button>
+            </div>
+            <div id="login_signup_image">
+                <a href="register_page.php">Login</a>
+                <a href="login_page.php">Sign Up</a>
+                <img src="/images/anonymous.png">
+            </div>
+        </header>
+        <section id=item>
+                <img src="/images/default.jpg" alt="imagem">
+            <section id=description>
+                <h1>
+                    Avião low cost (segunda mão)
+                </h1>
+                <h5>
+                aviao em segunda mao, em bom estado, tem 2 riscos na asa direita mas de resto está como novo
+                </h5>
+                <button>add to cart</button>
+                <footer>
+                    Ask me anything!
+                </footer>
+            </section>
+        </section>
+        
+        
 
-    
+        
 
-    <footer>
-        <p>&copy; Voyager International Commerce Group</p>
-    </footer>
-</body>
+        <footer>
+            <p>&copy; Voyager International Commerce Group</p>
+        </footer>
+    </body>
+</html>
