5043e2050649c7a45f1efb605d21580b1e9f5898
Afonso Castro <up202208026@up.pt>
2024-05-02 16:30:25 +0100

    adjustments to header

--- templates/item.tpl.php
+++ templates/item.tpl.php
@@ -8,14 +8,14 @@
 
 <?php function drawFeatured(array $items) { ?>
 <section id=featured>
-    <h1><a href="item.html">Featured Items</a></h1>
+    <h1><a>Featured Items</a></h1>
     <section id=featured_articles>
 
         <?php foreach($items as $item) { 
                 if($item->featured) {?>
 
             <article>
-                <h1><a href="item.html"><?=$item->name?></a></h1>
+                <h1><a href="item.php"><?=$item->name?></a></h1>
                 <img src="/images/defaults/default2.jpg" alt="default">
                 <footer>
                     <span class="price"><a href="item.php"><?=$item->price?></a></span>
@@ -33,14 +33,14 @@
 
 <?php function drawItems(array $items) { ?>
 <section id=items>
-    <h1><a href="item.html">Additional Items</a></h1>
+    <h1><a>Additional Items</a></h1>
     <section id=items_articles>
 
     <?php foreach($items as $item) { 
                 if(!$item->featured) {?>
 
             <article>
-                <h1><a href="item.html"><?=$item->name?></a></h1>
+                <h1><a href="item.php"><?=$item->name?></a></h1>
                 <img src="/images/defaults/default2.jpg" alt="default">
                 <footer>
                     <span class="price"><a href="item.php"><?=$item->price?></a></span>
