6fd4cb0cd72e5bb8105529fc8fb034a96ed36f2f
Afonso Castro <up202208026@up.pt>
2024-05-03 18:11:47 +0100

    Fixing small errors from last commit + profile picture file receiver

--- pages/register.php
+++ pages/register.php
@@ -21,16 +21,20 @@
         <form action="authenticate.php" method="post">
             <h2>Sign up today!</h2>
             <div class="input-container">
-                <label for="email">Username:</label>
+                <label for="email">Email:</label>
                 <input type="text" id="email" name="email" required>
             </div>
             <div class="input-container">
-                <label for="username">Password:</label>
+                <label for="username">Username:</label>
                 <input type="username" id="username" name="username" required>
             </div>
             <div class="input-container">
                 <label for="password">Password:</label>
                 <input type="password" id="password" name="password" required>
+            </div>
+            <div class="input-container">
+                <label for="profile_picture">Profile Picture:</label>
+                <input type="file" id="profile_picture" name="profile_picture" accept="image/png,image/jpeg">
             </div>
             <button type="submit">Create Account</button>
     </div>
