a6721364ca413ce3516af1a8dc1463493f8df2a1
António Romão <antonio.romao03@gmail.com>
2024-05-14 11:35:17 +0100

    Changed logout action

--- actions/logout.action.php
+++ actions/logout.action.php
@@ -2,7 +2,13 @@
     declare(strict_types = 1);
 
     require_once(dirname(__DIR__).'/database/session.class.php');
+
+
     $session = new Session();
-    $session->logout();
-    header('Location: /../pages/login.php');
+    
+    if ($session->isLoggedIn()) {
+        $session->logout();
+    }
+
+    header('Location: /../pages/index.php');
 ?>
