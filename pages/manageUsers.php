b6f8a73174c5371082d4f521ec7307ba750b5eea
pedroossantos <up202205900@up.pt>
2024-05-17 14:44:31 +0100

    Manage Users page half done

--- pages/manageUsers.php
+++ pages/manageUsers.php
@@ -2,18 +2,20 @@
     declare(strict_types = 1);
 
     require_once(__DIR__ . '/../database/connection.db.php');
+
     require_once(__DIR__ . '/../database/category.class.php');
     require_once(__DIR__ . '/../database/item.class.php');
     require_once(dirname(__DIR__).'/database/user.class.php');
+
     require_once(dirname(__DIR__).'/database/session.class.php');
+
     require_once(__DIR__ . '/../templates/common.tpl.php');
     require_once(__DIR__ . '/../templates/item.tpl.php');
 
     $db = getDatabaseConnection();
     $session = new Session();
     $categories = Category::getAllCategories($db);
-
-    $itemsForSale = Item::getItemsOfSellerUser($db,$session->getId());
+    $allUsers = User::getAllUsers($db);
     if ($session->isLoggedIn()) {$user = User::getUserFromId($db,$session->getId());}
 ?>
 
@@ -24,17 +26,25 @@
     <head>
         <title>Voyager</title>
         <meta charset="UTF-8">
-        <link href="/css/admin.css" rel="stylesheet">
+        <link href="/css/manageUsers.css" rel="stylesheet">
     </head>
     <body>
-        <?php drawHeader($session, $user);?>
-        <ul>
-            <span style="font-size: 1.5em;">Admin Option Thing</span>
-            <li><a href="createCategory.php">Add new Category</a></li>
-            <li><a href="manageUsers.php">Manage Users</a></li>
-        </ul>
 
-        
+        <?php drawHeader($session, $user);?>
+        <?php foreach ($allUsers as $person) { ?>
+            <section id="user">
+            <img src="/images/profilepictures/<?=$person->profilepicture ?>.png" alt="">
+            <h1><?= $person->name ?></h1>
+            <h2><?= $person->email ?></h2>
+            <?php if($person->isAdmin) : ?>
+                <h2>Admin</h2>
+            <?php else: ?>
+            <button>Promote to Admin</button>
+            <button>Ban</button>
+            <?php endif; ?>
+            </section>
+        <?php }?>
         <?php drawFooter();?>
+        
     </body>
 </html>
